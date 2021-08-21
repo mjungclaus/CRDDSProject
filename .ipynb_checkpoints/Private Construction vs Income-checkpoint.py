02961725ab3f6de6b14b79d4db03797afec7f64e
mjungclaus <49322029+mjungclaus@users.noreply.github.com>
2021-08-21 11:46:27 -0600

    Main push

---
+++ .ipynb_checkpoints/Private Construction vs Income-checkpoint.py
@@ -0,0 +1,12 @@
+# New project!
+
+# This is where we import new packages and whatnot:
+import csv
+
+import pandas as pd
+import csv
+
+# Let's import some data
+df=pd.read_csv('ACSST1Y2010.csv')
+print(df)
+
